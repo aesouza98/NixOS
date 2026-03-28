1d3cb21774e3b4f1ce3d4276ec34fa6e5e49cee9
Adriano Elias <github@nano.slmail.me>
2026-03-28 15:54:41 -0300

    specify niri config file

--- modules/features/niri.nix
+++ modules/features/niri.nix
@@ -27,36 +27,40 @@
       };
       packages.niri = inputs.wrapper-modules.wrappers.niri.wrap {
         inherit pkgs;
-        settings = {
-          spawn-at-startup = [
-            (lib.getExe self'.packages.noctalia)
-          ];
-          input.keyboard = {
-            xkb = {
-              layout = "us";
-              variant = "intl";
-            };
-          };
-
-          layout.gaps = 8;
-
-          outputs = {
-            DP-1 = {
-              mode = "2560x1440@155.000";
-              scale = 1;
-            };
-          };
-
-          binds = {
-            "Mod+Space".spawn-sh = "${lib.getExe self'.packages.noctalia} ipc call launcher toggle";
-            "Mod+Escape".spawn-sh = "${lib.getExe self'.packages.noctalia} ipc call sessionMenu toggle";
-            "Mod+Return".spawn-sh = lib.getExe pkgs.ghostty;
-            "Mod+E".spawn-sh = "${lib.getExe pkgs.nautilus} --new-window";
-            "Mod+W".close-window = null;
-            "Mod+O".spawn-sh = "${lib.getExe pkgs.obsidian}";
-            "Mod+B".spawn-sh = "${lib.getExe pkgs.brave}";
-          };
+        "config.kdl" = {
+          path = "~/.config/niri/config.kdl";
         };
+        # settings = {
+        #   spawn-at-startup = [
+        #     (lib.getExe self'.packages.noctalia)
+        #   ];
+        #   xwayland-satellite.path = lib.getExe pkgs.xwayland-satellite;
+        #   input.keyboard = {
+        #     xkb = {
+        #       layout = "us";
+        #       variant = "intl";
+        #     };
+        #   };
+
+        #   layout.gaps = 8;
+
+        #   outputs = {
+        #     DP-1 = {
+        #       mode = "2560x1440@155.000";
+        #       scale = 1;
+        #     };
+        #   };
+
+        #   binds = {
+        #     "Mod+Space".spawn-sh = "${lib.getExe self'.packages.noctalia} ipc call launcher toggle";
+        #     "Mod+Escape".spawn-sh = "${lib.getExe self'.packages.noctalia} ipc call sessionMenu toggle";
+        #     "Mod+Return".spawn-sh = lib.getExe pkgs.ghostty;
+        #     "Mod+E".spawn-sh = "${lib.getExe pkgs.nautilus} --new-window";
+        #     "Mod+W".close-window = null;
+        #     "Mod+O".spawn-sh = "${lib.getExe pkgs.obsidian}";
+        #     "Mod+B".spawn-sh = "${lib.getExe pkgs.brave}";
+        #   };
+        # };
       };
     };
 }
