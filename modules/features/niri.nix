35c721a1bd84cd47c4ba445f5f245f5e18db7cac
Adriano Elias <github@nano.slmail.me>
2026-03-29 16:45:25 -0300

    Update niri.nix

--- modules/features/niri.nix
+++ modules/features/niri.nix
@@ -95,12 +95,9 @@
             {
               matches = [ { title = "Picture.{0,1}in.{0,1}[Pp]icture"; } ];
               open-floating = true;
-              default-floating-position = {
-                x = 16;
-                y = 16;
-                relative-to = "bottom-right";
-              };
-
+              default-floating-position.x = 16;
+              default-floating-position.y = 16;
+              default-floating-position.relative-to = "bottom-right";
               open-focused = false;
             }
             {
