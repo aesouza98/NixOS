2e12f0fb3d4389fb54a8abc9de7322a4d9d6d352
Adriano Elias <github@nano.slmail.me>
2026-03-29 22:57:02 -0300

    Update niri.nix

--- modules/features/niri.nix
+++ modules/features/niri.nix
@@ -96,13 +96,9 @@
               matches = [ { title = "Picture.{0,1}in.{0,1}[Pp]icture"; } ];
               open-floating = true;
               open-focused = false;
-              default-floating-position = [
-                {
-                  x = 16;
-                  y = 16;
-                  relative-to = "bottom-right";
-                }
-              ];
+              default-floating-position.x = 16;
+              default-floating-position.y = 16;
+              default-floating-positionrelative-to = "bottom-right";
             }
             {
               geometry-corner-radius = 20;
