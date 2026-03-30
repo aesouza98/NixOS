86e22f52c61af0da8d397088f5e0b7634eaa16be
Adriano Elias <github@nano.slmail.me>
2026-03-30 00:00:41 -0300

    add amaisdao asdas

---
+++ modules/features/programs/niri.nix
@@ -0,0 +1,13 @@
+{ self, inputs, ... }:
+{
+
+  flake.nixosModules.niri =
+    { pkgs, ... }:
+    {
+      programs.niri = {
+        enable = true;
+        useNautilus = true;
+      };
+      environment.systemPackages = pkgs.noctalia-shell;
+    };
+}
