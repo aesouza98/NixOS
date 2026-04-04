ad552b2b28926cadcbb8063e4467ac4a0fdfa32f
Adriano Elias <github@nano.slmail.me>
2026-04-04 12:58:26 -0300

    fix fish

--- modules/features/programs/fish.nix
+++ modules/features/programs/fish.nix
@@ -2,11 +2,16 @@
 {
 
   flake.nixosModules.fish =
-    { ... }:
+    { pkgs, ... }:
     {
       programs.fish = {
         enable = true;
         generateCompletions = true;
       };
+      environment.systemPackages = with pkgs.fishPlugins; [
+        fzf-fish
+        async-prompt
+        pure
+      ];
     };
 }
