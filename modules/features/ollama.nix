efa9fae6d7a3e6226e49d341ac4adb62d72cc537
Adriano Elias <github@nano.slmail.me>
2026-04-09 20:58:30 -0300

    add ollama

--- modules/features/ollama.nix
+++ modules/features/ollama.nix
@@ -4,19 +4,29 @@
   flake.nixosModules.ollama =
     { pkgs, ... }:
     {
+      nix.settings = {
+        substituters = [
+          "https://cuda-maintainers.cachix.org"
+        ];
+        trusted-public-keys = [
+          "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+        ];
+      };
 
       hardware.graphics.enable = true;
       services.xserver.videoDrivers = [ "nvidia" ];
 
       services.ollama = {
         enable = true;
-        acceleration = "cuda"; # força o uso da gpu
+        package = pkgs.ollama-cuda;
       };
 
       environment.systemPackages = with pkgs; [
         ollama
-        aider-chat # melhor CLI para codificação (lê seus arquivos locais)
+        # claude-code
+        # aider-chat # melhor CLI para codificação (lê seus arquivos locais)
         oterm # cliente de chat bonito direto no terminal (opcional)
+        litellm
       ];
 
       users.users.nano = {
