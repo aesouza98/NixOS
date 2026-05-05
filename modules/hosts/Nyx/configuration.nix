0883cee527847930c90f95fd196930bd7359d36f
Adriano Elias <63488275+aesouza98@users.noreply.github.com>
2026-05-05 20:17:45 -0300

    Switch from GNOME to PLASMA Desktop Environment

--- modules/hosts/Nyx/configuration.nix
+++ modules/hosts/Nyx/configuration.nix
@@ -58,9 +58,15 @@
       # Enable the X11 windowing system.
       services.xserver.enable = true;
 
-      # Enable the GNOME Desktop Environment.
-      services.xserver.displayManager.gdm.enable = true;
-      services.xserver.desktopManager.gnome.enable = true;
+      # Enable the PLASMA Desktop Environment.
+      services.desktopManager.plasma6.enable = true;
+      services.displayManager.sddm = {
+        enable = true;
+        wayland.enable = true;
+      };
+
+      #services.xserver.displayManager.gdm.enable = true;
+      #services.xserver.desktopManager.gnome.enable = true;
 
       # Configure keymap in X11
       services.xserver.xkb = {
